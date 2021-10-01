842fba017e69013d4640d4235c826a66b739e5a6
Raverside <r4verside@gmail.com>
2021-10-01 17:54:43 +0300

    force MD ionic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {IonApp, IonRouterOutlet} from '@ionic/react';
+import {IonApp, IonRouterOutlet, setupConfig} from '@ionic/react';
 import {IonReactRouter} from '@ionic/react-router';
 import {Route, Redirect} from 'react-router-dom';
 import Menu from './components/Menu/Menu';
@@ -43,6 +43,8 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 import './theme/global-styles.css';
+
+setupConfig({mode: 'md'});
 
 const App: React.FC = () => {
 
