eeafc36279c011b426a4cbf91e3113dc2f7cb301
Raverside <r4verside@gmail.com>
2021-11-07 05:09:42 +0200

    updated contact us page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -6,7 +6,6 @@ import {
     IonText,
 } from '@ionic/react';
 import {paperPlane as paperPlaneIcon} from "ionicons/icons";
-import {logoYoutube as youtubeIcon} from "ionicons/icons";
 import React from "react";
 import ArrowHeader from '../components/Header/ArrowHeader';
 
@@ -19,15 +18,10 @@ const Contact: React.FC = () => {
             <ArrowHeader title="Contact Us" />
             <IonContent fullscreen id="contact-content">
                 <div className="contact-wrapper">
-                    <IonText className="contact-text">Reach out to us on these platforms</IonText>
-                    <div className="contact-option">
-                        <div className="contact-option_label"><IonIcon icon={paperPlaneIcon} color="tertiary" style={{marginRight: "10px", fontSize: "14px"}} /> Telegram</div>
-                        <IonButton fill="clear" color="dark" href="https://telegram.me/raverside" target="_blank">+1 (849) 986 4545</IonButton>
-                    </div>
-                    <div className="contact-option">
-                        <div className="contact-option_label"><IonIcon icon={youtubeIcon} color="primary" style={{marginRight: "10px", fontSize: "14px"}} /> Youtube</div>
-                        <IonButton fill="clear" color="dark" href="https://www.youtube.com/c/GallosClub" target="_blank">Gallos Club</IonButton>
-                    </div>
+                    <IonText className="restore-passcode-text">Write to us on Telegram if you have any inquiries or suggestions</IonText>
+                    <IonButton expand="block" color="tertiary" href="https://telegram.me/gallosclub" target="_blank">
+                        <IonIcon icon={paperPlaneIcon} style={{marginRight: "10px", fontSize: "14px"}} /> Contact Us
+                    </IonButton>
                 </div>
             </IonContent>
         </IonPage>
