a937935099946344f7d9c58799213823a65895b3
Raverside <r4verside@gmail.com>
2021-12-09 05:43:13 +0200

    websocket path config

--- src/State.tsx
+++ src/State.tsx
@@ -2,6 +2,7 @@ import React, { createContext, useReducer } from "react";
 import Cookies from "js-cookie";
 import { io } from "socket.io-client";
 const socket = io(process.env.REACT_APP_NODE_API_URL!, {
+    path: process.env.REACT_APP_WEBSOCKET_PATH,
     transports: ['websocket'],
     rejectUnauthorized: false
 });
