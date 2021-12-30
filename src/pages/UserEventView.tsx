2b6a727633b009844c35c843e5e1afed533b1195
Raverside <r4verside@gmail.com>
2021-12-30 11:16:07 +0200

    manually created matches go at the bottom for Regular Users now

--- src/pages/UserEventView.tsx
+++ src/pages/UserEventView.tsx
@@ -142,7 +142,7 @@ const UserEventView: React.FC = () => {
     const title = (event?.is_special && event?.title) ? event?.title! : t('events.default_event_name');
     const image = (event?.is_special && event?.image) ? getImageUrl(event?.image!) : getImageUrl(event?.stadium_image!);
     const matches = (event && baloteoSearch) ? event.matches?.filter((m:any) => +m.participant?.cage === +baloteoSearch || +m.opponent?.cage === +baloteoSearch || m.participant?.team?.name.toLowerCase().includes(baloteoSearch.toLowerCase()) || m.opponent?.team?.name.toLowerCase().includes(baloteoSearch.toLowerCase())) : event?.matches;
-    const liveMatches = matches?.filter((m:any) => m.live) || [];
+    const liveMatches = matches?.filter((m:any) => m.live).sort((a:any, b:any) => a.manual - b.manual) || [];
     const completeMatches = matches?.filter((m:any) => !m.live && m.result !== null) || [];
     const activeMatches = (baloteoTab === "results") ? completeMatches : liveMatches;
 
